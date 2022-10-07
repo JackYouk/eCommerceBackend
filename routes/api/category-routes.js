e58a3891ba8630507948d0c51e85aded9b5bebb2
JackYouk <jackyoukstetter1@gmail.com>
2022-10-07 00:29:40 -0700

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,21 +19,68 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+        },
+      ]
+    });
+    if (!category) {
+      res.status(404).json({ error: 'cant find this id' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  // {category_name: 'Cat Litter'}
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    const category = await Category.findByPk(req.params.id);
+    res.status(200).json({ updated: category });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+      res.status(404).json({ error: 'No category found with this ID!' });
+      return;
+    }
+    await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json({deleted: category});
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
